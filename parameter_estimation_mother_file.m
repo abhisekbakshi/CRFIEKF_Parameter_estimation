5be6be3c12fcd429bf12160c5194d64b427738b1
ABHISEK BAKSHI <abhisek.bakshi@gmail.com>
2022-03-31 12:36:07 +0530

    Add files via upload

---
+++ parameter_estimation_mother_file.m
@@ -0,0 +1,21 @@
+
+%%% Run Parameter estimation code of General glycolysis pathway parameter estimation
+run(pwd+"\General glycolysis pathway parameter estimation\Performance test with different FIS output\Glycolysis_Parameter_estimation_final.m");
+run(pwd+"\General glycolysis pathway parameter estimation\Performance test with different membership function\Glycolysis_Parameter_estimation_final.m");
+run(pwd+"\General glycolysis pathway parameter estimation\Performance test with different regularization\Glycolysis_Parameter_estimation_final.m");
+
+%%% Run Generic branched pathway model parameter estimation
+run(pwd+"\Generic branched pathway model parameter estimation\BPM_parameter_estimation.m");
+
+%%% Run IL-6-dependent Jak-STAT3 parameter estimation
+run(pwd+"\IL-6-dependent Jak-STAT3 parameter estimation\IL6_jak_STAT3_Parameter_estimation_final.m");
+
+%%% Run JAK-STAT pathway parameter estimation
+run(pwd+"\JAK-STAT pathway parameter estimation\JAK_STAT_parameter_estimation.m");
+
+%%% Run Ras pathway parameter estimation
+run(pwd+"\Ras pathway parameter estimation\Ras_model_parameter_estimation.m");
+
+%%% Run Yeast glycolysis pathway
+run(pwd+"\Yeast glycolysis pathway\Yeast_Parameter_estimation.m");
+
